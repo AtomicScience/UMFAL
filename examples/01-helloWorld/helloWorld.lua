e100bb32d5af3978251697c68a37f4411886db9b
AtomicScience <dmitry.greblyuk@mail.ru>
2021-07-26 21:40:51 +0300

    Comments from 1st example removed to be replaced by README

--- examples/01-helloWorld/helloWorld.lua
+++ examples/01-helloWorld/helloWorld.lua
@@ -1,10 +1,7 @@
--- An empty module is created
 local helloWorld = {}
 
--- A function is added to the module
 function helloWorld.sayHelloWorld()
     print("Hello World!")
 end
 
--- Returning a module
 return helloWorld
--- examples/01-helloWorld/init.lua
+++ examples/01-helloWorld/init.lua
@@ -1,13 +1,3 @@
--- In addition to simply requiring the UMFAL as a library, one should also
--- INIT AN APPLICATION by calling an initAppFromRelative function
---
--- The string argument for the said function is the IDENTIFIER - a token that
--- can be later used by other modules to fetch an already initialized applications later
--- (this feature of UMFAL is covered in example #3 "Application Fetch")
---
--- The reason IDENTIFIER in this example is absurdly long is the desire to demonstrate
--- that IDENTIFIER can be any string you want, with the only limitation - it MUST be unique
--- between different UMFAL apps.
 local helloWorldApp = require("umfal").initAppFromRelative("helloWorldExampleForMyAwesomeOpenComputersLibraryCalledUMFAL")
 
 helloWorldApp.helloWorld.sayHelloWorld();
